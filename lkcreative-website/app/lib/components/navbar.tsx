c27ad32a259ccbd1153b6c9ada5dff813b6dfe54
Bryce Anderson <abryce.anderson@gmail.com>
2023-07-19 21:24:01 -0400

    navbar started and added to layout

---
+++ lkcreative-website/app/lib/components/navbar.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+const Navbar = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      <h2>About Us</h2>
+      <h2>Portfolio</h2>
+      <h2>Contact</h2>
+    </div>
+  )
+}
+
+export default Navbar;
