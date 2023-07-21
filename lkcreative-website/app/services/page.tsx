2ea0d3f9daa39403518068bd03de705c84ace179
Bryce Anderson <abryce.anderson@gmail.com>
2023-07-20 21:15:57 -0400

    nav bar navigation works and refactored to list with links

---
+++ lkcreative-website/app/services/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Services() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        Services
+      </div>
+    </main>
+  )
+}
